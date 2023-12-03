b0df41bd350e5a812ff78cd062f36fbbb40bf12e
Johan <davidbm270@gmail.com>
2023-12-03 00:21:06 -0500

    alo

--- app/src/main/java/com/example/productcrud/RetrofitHelper.kt
+++ app/src/main/java/com/example/productcrud/RetrofitHelper.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitHelper {
-    private const val BASE_URL = "http://192.168.18.5:8000"
+    private const val BASE_URL = "http://192.168.18.233:8000"
 
     fun getInstance(): Retrofit {
         return Retrofit.Builder()
